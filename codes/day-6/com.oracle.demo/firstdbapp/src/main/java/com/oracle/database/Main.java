d246729cdf0c7c0349ca4c656049eaff8881427c
joydip-git <joydip.mondal@gmail.com>
2025-09-08 14:53:19 +0530

    fething data through repository and model is completed

--- codes/day-6/com.oracle.demo/firstdbapp/src/main/java/com/oracle/database/Main.java
+++ codes/day-6/com.oracle.demo/firstdbapp/src/main/java/com/oracle/database/Main.java
@@ -1,10 +1,34 @@
 package com.oracle.database;
 
 import java.sql.SQLException;
+import java.util.Scanner;
+
+import com.oracle.database.models.Product;
+import com.oracle.database.repository.ProductRepository;
 
 public class Main {
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        
+        ProductRepository repository = new ProductRepository();
+        // List<Product> products = repository.getAll();
+        // if (products != null && !products.isEmpty()) {
+        //     // Consumer<Product> printProduct = (p) -> System.out.println(p);
+        //     // products.forEach(printProduct);
+        //     products.forEach((p) -> System.out.println(p));
+        // } else {
+        //     System.out.println("no products found...");
+        // }
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter product id: ");
+        int pid = scanner.nextInt();
+        Product product = repository.get(pid);
+        if (product != null) {
+            System.out.println(product);
+        } else {
+            System.out.println("no product found...");
+        }
+
+        scanner.close();
     }
 }
