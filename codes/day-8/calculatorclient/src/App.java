0ede8683257664126b063d19d218ebd5e2295cf7
joydip-git <joydip.mondal@gmail.com>
2025-09-10 13:10:37 +0530

    J2EE, JakartaEE and REST

---
+++ codes/day-8/calculatorclient/src/App.java
@@ -0,0 +1,8 @@
+import com.oracle.calculation.Calculator;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.add(12, 3));
+    }
+}
