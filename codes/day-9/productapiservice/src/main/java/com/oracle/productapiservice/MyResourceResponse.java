cc5e46c793b76012edede2bfe2c368b621432ffa
joydip-git <joydip.mondal@gmail.com>
2025-09-11 15:30:03 +0530

    RESTful API complete example

---
+++ codes/day-9/productapiservice/src/main/java/com/oracle/productapiservice/MyResourceResponse.java
@@ -0,0 +1,33 @@
+package com.oracle.productapiservice;
+
+public class MyResourceResponse<T> {
+	private String message;
+	private int statusCode;
+	private T data;
+	public MyResourceResponse() {
+	}
+	public MyResourceResponse(String message, int statusCode, T data) {
+		this.message = message;
+		this.statusCode = statusCode;
+		this.data = data;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public int getStatusCode() {
+		return statusCode;
+	}
+	public void setStatusCode(int statusCode) {
+		this.statusCode = statusCode;
+	}
+	public T getData() {
+		return data;
+	}
+	public void setData(T data) {
+		this.data = data;
+	}
+	
+}
